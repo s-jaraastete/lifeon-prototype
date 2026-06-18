11be96a9aa4cdc934f4de356d781b244ae0d3fe5
Pablo Yañez <pablo.yanez1@safetyclub.cl>
2026-06-18 16:56:59 -0400

    feat: add ContactForm component and integrate it into the ContactPage

---
+++ app/contacto/components/ContactForm.tsx
@@ -0,0 +1,49 @@
+import Link from "next/link";
+import TextInput from "../../components/TextInput";
+
+const ContactForm = () => {
+  return (
+    <form className="bg-white p-7.5 rounded-[14px]">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-6">
+        <TextInput
+          label="Nombre"
+          placeholder="Tu nombre"
+        />
+        <TextInput
+          label="Apellidos"
+          placeholder="Tus apellidos"
+        />
+        <TextInput
+          label="Correo electrónico"
+          placeholder="Tu correo electrónico"
+          type="email"
+        />
+        <TextInput
+          label="Teléfono"
+          placeholder="+56"
+          type="tel"
+        />
+      </div>
+      <div className="mb-6">
+        <TextInput
+          label="Empresa"
+          placeholder="Nombre de tu empresa"
+        />
+      </div>
+      <button className="w-full font-medium bg-primary px-4 py-3 rounded-xl text-white hover:bg-red-600 transition duration-200 cursor-pointer">
+        Enviar mensaje
+      </button>
+      <p className="mt-6 text-[12px] text-primary-text text-center">
+        Al enviar tu información, autorizas a LifeOn a procesar tus datos
+        personales para fines comerciales y de contacto relacionados con
+        nuestros productos y servicios, conforme a nuestra{" "}
+        <Link href="/" className="underline">
+          Política de Privacidad
+        </Link>
+        .
+      </p>
+    </form>
+  );
+};
+
+export default ContactForm;
---
+++ app/contacto/page.tsx
@@ -0,0 +1,21 @@
+import ContactForm from "./components/ContactForm";
+
+export default function ContactPage() {
+  return (
+    <div className="w-full px-6 py-16 bg-gray-200">
+      <div className="flex flex-col mx-auto max-w-[1300px] lg:flex-row gap-15">
+        <div className="flex-1 flex flex-col">
+          <h1 className="text-5xl font-bold mb-2.5">
+            Completa el formulario y te contactaremos pronto
+          </h1>
+          <p className="text-[24px] leading-7.5 text-primary-text">
+            Un especialista de nuestro equipo se comunicará contigo para responder tus dudas, entender tus necesidades y ayudarte a avanzar con total claridad.
+          </p>
+        </div>
+        <div className="flex-1">
+          <ContactForm />
+        </div>
+      </div>
+    </div>
+  )
+}
