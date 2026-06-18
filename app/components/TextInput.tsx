3b444800384a7d49a30e03831180237100bb0796
Pablo Yañez <pablo.yanez1@safetyclub.cl>
2026-06-18 16:28:22 -0400

    feat: add TextInput component with label and styling

---
+++ app/components/TextInput.tsx
@@ -0,0 +1,41 @@
+import {DetailedHTMLProps, InputHTMLAttributes} from "react";
+
+export interface TextInputProps extends DetailedHTMLProps<
+  InputHTMLAttributes<HTMLInputElement>,
+  HTMLInputElement
+> {
+  label?: string;
+  className?: string;
+}
+
+const TextInput = (props: TextInputProps) => {
+  const {
+    label,
+    className,
+    ...inputProps
+  } = props;
+
+  return (
+    <div className="flex flex-col gap-2.5">
+      {label && (
+        <label className="text-lg font-medium leading-6">
+          {props.label}
+          {props.required && <span className="text-red-600 ml-0.5">*</span>}
+        </label>
+      )}
+
+      <input
+        className={`
+          appearance-none w-full py-3 px-5
+          transition duration-200 focus:ring-1 focus:ring-primary focus:outline-none
+          rounded-xl ring-1 ring-gray-400 placeholder-gray-700
+          ${props.disabled ? "cursor-not-allowed" : ""}
+          ${className ?? ""}
+        `}
+        {...inputProps}
+      />
+    </div>
+  );
+}
+
+export default TextInput
