c237700c6f1c8d2108fe5e2f143f639cbf8d13b7
Pablo Yañez <pablo.yanez1@safetyclub.cl>
2026-06-19 16:55:27 -0400

    feat: hide footer link sections in basket and checkout flows

--- app/components/layout/Footer.tsx
+++ app/components/layout/Footer.tsx
@@ -1,20 +1,35 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { FaInstagram, FaLinkedin, FaYoutube } from "react-icons/fa";
 
 const Footer = () => {
+  const pathname = usePathname();
+  const compact = pathname.startsWith("/basket") || pathname.startsWith("/checkout");
+
   return (
     <footer className="flex flex-col mx-auto w-full h-full">
       <div className="w-full bg-secondary text-white px-4">
-        {/* TODO: Verify responsive layout for mobile */}
-        <div className="flex flex-col py-[50px] lg:flex lg:py-20 h-full mx-auto max-w-[1300px]">
-          <div className="text-center lg:text-left flex flex-col lg:flex-row justify-between gap-6 pb-10 w-full border-b border-teal-400">
+        <div className="flex flex-col py-[50px] lg:flex lg:py-10 h-full mx-auto max-w-[1300px]">
+          <div
+            className={`
+            text-center lg:text-left flex flex-col lg:flex-row gap-6 pb-10 w-full border-b border-teal-400
+            ${compact ? "justify-center" : "justify-between lg:pt-10"}`}
+          >
             <div className="flex flex-col items-center lg:items-start space-y-7 mb-6 lg:mb-0">
               <Link href="/">
-                <h3 className="text-[40px] font-semibold">Life
+                <h3 className="text-[40px] font-semibold">
+                  Life
                   <span className="font-extrabold">On</span>
                 </h3>
               </Link>
-              <div className="flex items-center space-x-2.5">
+              <div
+                className={`
+                flex items-center space-x-2.5
+                ${compact ? "justify-center w-full" : "justify-start"}
+              `}
+              >
                 <Link
                   href="https://instagram.com"
                   target="_blank"
@@ -42,52 +57,56 @@ const Footer = () => {
               </div>
             </div>
 
-            <div className="mb-6 lg:mb-0 text-center lg:text-left">
-              <h6 className="font-semibold text-[18px] mb-3">Módulos</h6>
-              <div className="lg:columns-2 lg:gap-x-16 xl:gap-x-36 [&_a]:inline-block [&_a]:max-w-62">
-                <ul className="space-y-2.5">
-                  <li>
-                    <Link href="/">MIPER</Link>
-                  </li>
-                  <li>
-                    <Link href="/">Investigación de Accidentes</Link>
-                  </li>
-                  <li>
-                    <Link href="/">Process Safety</Link>
-                  </li>
-                  <li>
-                    <Link href="/">Auditorías</Link>
-                  </li>
-                  <li>
-                    <Link href="/">Verificación de Controles Críticos</Link>
-                  </li>
-                  <li>
-                    <Link href="/">Asset Integrity e Inspecciones Estructurales</Link>
-                  </li>
-                  <li>
-                    <Link href="/">Gestión del Cambio</Link>
-                  </li>
-                  <li>
-                    <Link href="/">Búsqueda documental</Link>
-                  </li>
-                  <li>
-                    <Link href="/">Formaciones</Link>
-                  </li>
-                </ul>
-              </div>
-            </div>
+            {!compact && (
+              <>
+                <div className="mb-6 lg:mb-0 text-center lg:text-left">
+                  <h6 className="font-semibold text-[18px] mb-3">Módulos</h6>
+                  <div className="lg:columns-2 lg:gap-x-16 xl:gap-x-36 [&_a]:inline-block [&_a]:max-w-62">
+                    <ul className="space-y-2.5">
+                      <li>
+                        <Link href="/">MIPER</Link>
+                      </li>
+                      <li>
+                        <Link href="/">Investigación de Accidentes</Link>
+                      </li>
+                      <li>
+                        <Link href="/">Process Safety</Link>
+                      </li>
+                      <li>
+                        <Link href="/">Auditorías</Link>
+                      </li>
+                      <li>
+                        <Link href="/">Verificación de Controles Críticos</Link>
+                      </li>
+                      <li>
+                        <Link href="/">Asset Integrity e Inspecciones Estructurales</Link>
+                      </li>
+                      <li>
+                        <Link href="/">Gestión del Cambio</Link>
+                      </li>
+                      <li>
+                        <Link href="/">Búsqueda documental</Link>
+                      </li>
+                      <li>
+                        <Link href="/">Formaciones</Link>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
 
-            <div className="mb-6 lg:mb-0 text-center lg:text-left">
-              <h6 className="font-semibold text-[18px] mb-3">Soporte</h6>
-              <ul className="space-y-2.5 [&_a]:inline-block [&_a]:max-w-62">
-                <li>
-                  <Link href="/">Centro de ayuda</Link>
-                </li>
-                <li>
-                  <Link href="/">Contacto</Link>
-                </li>
-              </ul>
-            </div>
+                <div className="mb-6 lg:mb-0 text-center lg:text-left">
+                  <h6 className="font-semibold text-[18px] mb-3">Soporte</h6>
+                  <ul className="space-y-2.5 [&_a]:inline-block [&_a]:max-w-62">
+                    <li>
+                      <Link href="/">Centro de ayuda</Link>
+                    </li>
+                    <li>
+                      <Link href="/">Contacto</Link>
+                    </li>
+                  </ul>
+                </div>
+              </>
+            )}
           </div>
 
           <div className="flex flex-col-reverse items-center text-center lg:flex-row lg:justify-between lg:items-center lg:text-left pt-10 space-y-4 lg:space-y-0">
