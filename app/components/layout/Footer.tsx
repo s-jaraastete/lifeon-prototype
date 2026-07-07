e85fd9d9508a1ec78e57474f4e909f2bae8ff388
Pablo Yañez <pablo.yanez1@safetyclub.cl>
2026-07-07 12:05:55 -0400

    feat: footer component redesign

--- app/components/layout/Footer.tsx
+++ app/components/layout/Footer.tsx
@@ -10,15 +10,19 @@ const Footer = () => {
 
   return (
     <footer className="flex flex-col mx-auto w-full h-full">
-      <div className="w-full bg-secondary text-white px-4">
-        <div className="flex flex-col py-[50px] lg:flex lg:py-10 h-full mx-auto max-w-[1300px]">
+      <div className="w-full bg-secondary text-white px-6.5">
+        <div className="flex flex-col py-20 lg:flex h-full mx-auto max-w-325">
           <div
             className={`
-            text-center lg:text-left flex flex-col lg:flex-row gap-6 pb-10 w-full border-b border-teal-400
-            ${compact ? "justify-center" : "justify-between lg:pt-10"}`}
+              lg:text-left flex flex-col lg:flex-row justify-between gap-6
+              text-center pb-10 w-full border-b border-teal-400
+            `}
           >
-            <div className="flex flex-col items-center lg:items-start space-y-7 mb-6 lg:mb-0">
-              <Link href="/">
+            <div className="flex flex-col items-center lg:items-start space-y-7 mb-7.5">
+              <Link
+                href="/"
+                className="mb-3.5 lg:mb-7.5"
+              >
                 <h3 className="text-[40px] font-semibold">
                   Life
                   <span className="font-extrabold">On</span>
@@ -58,45 +62,25 @@ const Footer = () => {
             </div>
 
             {!compact && (
-              <>
-                <div className="mb-6 lg:mb-0 text-center lg:text-left">
+              <div className="flex flex-col lg:flex-row gap-6 lg:gap-25">
+                <div className="mb-0 text-center lg:text-left">
                   <h6 className="font-semibold text-[18px] mb-3">Módulos</h6>
-                  <div className="lg:columns-2 lg:gap-x-16 xl:gap-x-36 [&_a]:inline-block [&_a]:max-w-62">
-                    <ul className="space-y-2.5">
-                      <li>
-                        <Link href="/">MIPER</Link>
-                      </li>
-                      <li>
-                        <Link href="/">Investigación de Accidentes</Link>
-                      </li>
-                      <li>
-                        <Link href="/">Process Safety</Link>
-                      </li>
-                      <li>
-                        <Link href="/">Auditorías</Link>
-                      </li>
-                      <li>
-                        <Link href="/">Verificación de Controles Críticos</Link>
-                      </li>
-                      <li>
-                        <Link href="/">Asset Integrity e Inspecciones Estructurales</Link>
-                      </li>
-                      <li>
-                        <Link href="/">Gestión del Cambio</Link>
-                      </li>
-                      <li>
-                        <Link href="/">Búsqueda documental</Link>
-                      </li>
-                      <li>
-                        <Link href="/">Formaciones</Link>
-                      </li>
-                    </ul>
-                  </div>
+                  <ul className="space-y-2.5">
+                    <li>
+                      <Link href="/">MIPER</Link>
+                    </li>
+                    <li>
+                      <Link href="/">Programa y Documentación Preventiva</Link>
+                    </li>
+                    <li>
+                      <Link href="/">APR Virtual</Link>
+                    </li>
+                  </ul>
                 </div>
 
-                <div className="mb-6 lg:mb-0 text-center lg:text-left">
+                <div className="mb-0 text-center lg:text-left">
                   <h6 className="font-semibold text-[18px] mb-3">Soporte</h6>
-                  <ul className="space-y-2.5 [&_a]:inline-block [&_a]:max-w-62">
+                  <ul className="space-y-2.5">
                     <li>
                       <Link href="/">Centro de ayuda</Link>
                     </li>
@@ -105,16 +89,16 @@ const Footer = () => {
                     </li>
                   </ul>
                 </div>
-              </>
+              </div>
             )}
           </div>
 
-          <div className="flex flex-col-reverse items-center text-center lg:flex-row lg:justify-between lg:items-center lg:text-left pt-10 space-y-4 lg:space-y-0">
+          <div className="flex flex-col items-center text-center lg:flex-row lg:justify-between lg:items-center lg:text-left pt-10 space-y-4 lg:space-y-0">
             <p className="text-[15px] lg:text-[16px]">
               © 2026 LifeOn, un producto de Technologies by SafetyCo. Todos los
               derechos reservados.
             </p>
-            <ul className="flex flex-col mb-[50px] lg:mb-0 lg:flex-row space-y-2 lg:space-y-0 lg:space-x-5.5 text-[14px] ">
+            <ul className="flex gap-5.5 space-y-2 lg:space-y-0 lg:space-x-5.5 text-[14px] ">
               <li>
                 <Link href="/">Términos de uso</Link>
               </li>
