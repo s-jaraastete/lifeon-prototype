3c367df65566fbee6b4a73f39a960fbc68f73e3d
Sergio A. Jara Astete <sergio.jara@safetyclub.cl>
2026-08-18 10:22:09 -0400

    Restaurar configuración Next.js para despliegue dinámico

--- next.config.ts
+++ next.config.ts
@@ -5,18 +5,6 @@ const nextConfig: NextConfig = {
     backendHost:
       process.env.NEXT_PUBLIC_BACKEND_HOST ?? "http://localhost:8001",
   },
-  /* images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "8001",
-        pathname: "/**",
-      },
-      { protocol: 'https', hostname: '*.lms.safetyacademy.cl', pathname: '/**' }
-    ],
-    dangerouslyAllowLocalIP: true,
-  }, */
 };
 
 export default nextConfig;
