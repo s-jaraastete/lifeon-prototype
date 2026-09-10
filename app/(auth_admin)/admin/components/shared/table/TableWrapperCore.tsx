4d1e3ec381e4a33e271c80efc693a14c51878b1c
Gonzalo Beristain <ga_beristainn@hotmail.com>
2026-09-10 17:41:05 -0300

    fix: table column

--- app/(auth_admin)/admin/components/shared/table/TableWrapperCore.tsx
+++ app/(auth_admin)/admin/components/shared/table/TableWrapperCore.tsx
@@ -1,7 +1,6 @@
 import LoadingState from '@/components/reusable/LoadingState'
 import clsx from 'clsx'
 import { Fragment, ReactNode } from 'react'
-import TableRoot from './TableRoot'
 
 type TableWrapperCoreProps<T> = {
   headers: (string | {
@@ -19,21 +18,20 @@ type TableWrapperCoreProps<T> = {
 const TableWrapperCore = <T, >(props: TableWrapperCoreProps<T>) => {
   return (
     <div className='overflow-x-auto rounded-lg border border-gray-300 bg-white'>
-        <TableRoot
-          stickyRightColumns={props.stickyRightColumns}
-          className={clsx(
-            'w-full',
-            props.stickyLastColumns === 1 && 'sticky-last-column',
-            props.stickyLastColumns === 2 && 'sticky-last-two-columns',
-          )}
-        >
+      <table
+        className={clsx(
+          'w-full',
+          props.stickyLastColumns === 1 && 'sticky-last-column',
+          props.stickyLastColumns === 2 && 'sticky-last-two-columns',
+        )}
+      >
         <thead>
           <tr>
             {props.headers.map((header, idx) => 
               <th
                 key={typeof(header) === 'string' ? header : header.label}
                 className={clsx(
-                'h-12.5whitespace-nowrap border-b border-gray-300 px-5 py-2.5 text-left text-body-sm font-medium leading-5.5 text-gray-950',
+                'h-12.5 whitespace-nowrap border-b border-gray-300 px-5 py-2.5 text-left text-body-sm font-medium leading-5.5 text-gray-950',
                 !props.stickyLastColumns
                   && idx === props.headers.length - 1
                   && (typeof header === 'string' || !header.className)
@@ -70,7 +68,7 @@ const TableWrapperCore = <T, >(props: TableWrapperCoreProps<T>) => {
             ))
           )}
         </tbody>
-        </TableRoot>
+      </table>
     </div>
   )
 }
